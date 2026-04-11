f57275d3aa4a740803a453cfc22c090293be9c90
Your Name <you@example.com>
2026-04-11 12:18:31 -0600

    agregando listado

--- src/main/java/com/trips/controllers/HomeControllers.java
+++ src/main/java/com/trips/controllers/HomeControllers.java
@@ -1,6 +1,8 @@
 package com.trips.controllers;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller	
 public class HomeControllers {
+	
 	
 	@GetMapping("/")
 	public String mostrarHome (Model model) {
@@ -25,5 +28,17 @@ public class HomeControllers {
 		
 		return "home";
 		
+	}
+	@GetMapping("/listado")
+	public String mostrarListado(Model model) {
+		
+		List<String> lista = new LinkedList<String>();
+		lista.add("En la Montaña");
+		lista.add("En la Ciudad");
+		lista.add("En los pueblos");
+		lista.add("En las playas");
+		model.addAttribute("ListadoTrips", lista);
+		
+		return "listado";
 	}
 }
