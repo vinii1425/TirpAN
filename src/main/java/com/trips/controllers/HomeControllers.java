10cbe5591066b932e3fa201813e5fe920eeb815d
Your Name <you@example.com>
2026-04-25 12:32:25 -0600

    Agregagamos servicios

--- src/main/java/com/trips/controllers/HomeControllers.java
+++ src/main/java/com/trips/controllers/HomeControllers.java
@@ -1,16 +1,18 @@
 package com.trips.controllers;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
+
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.trips.models.Trip;
+import com.trips.services.ITripServices;
 
 
 
@@ -18,6 +20,11 @@ import com.trips.models.Trip;
 
 @Controller	
 public class HomeControllers {
+	
+	
+	@Autowired
+	private ITripServices tripServices;
+	
 	
 	
 	@GetMapping("/")
@@ -61,67 +68,17 @@ public class HomeControllers {
 			return "detalle";
 		}
 		
-		private List<Trip> getTrips() {
-		    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-		    List<Trip> lista = new LinkedList<Trip>();
-
-		    try {
-		        Trip trip1 = new Trip();
-		        trip1.setId(1);
-		        trip1.setNomTrip("Rapel en Volcatenango");
-		        trip1.setDescripcion("Hacer rapel en los circuitos de Volcatenango");
-		        trip1.setFecha(sdf.parse("10-05-2022"));
-		        trip1.setCosto(5.0);
-		        trip1.setDestacado(1);
-		        trip1.setImagen("trip01.png"); 
-
-		        Trip trip2 = new Trip();
-		        trip2.setId(2);	
-		        trip2.setNomTrip("Deslizador en El picnic");
-		        trip2.setDescripcion("Deslizarte en un divertido tobogan sobre la colina");
-		        trip2.setFecha(sdf.parse("10-05-2022"));
-		        trip2.setCosto(5.0);
-		        trip2.setDestacado(0);
-		        trip2.setImagen("trip02.png"); 
-
-		        Trip trip3 = new Trip();
-		        trip3.setId(3);
-		        trip3.setNomTrip("Comida y Flores");
-		        trip3.setDescripcion("Disfrutar de un amplio jardin el cual podras comprar");
-		        trip3.setFecha(sdf.parse("10-05-2022"));
-		        trip3.setCosto(1.0);
-		        trip3.setDestacado(0);
-		        trip3.setImagen("trip03.png"); 
-
-		        Trip trip4 = new Trip();
-		        trip4.setId(4);
-		        trip4.setNomTrip("Caminatas");
-		        trip4.setDescripcion("Disfruta hacer senderismo por las montañas chalatecas");
-		        trip4.setFecha(sdf.parse("01-02-2022"));
-		        trip4.setCosto(1.0);
-		        trip4.setDestacado(1);
-		        trip4.setImagen("trip01.png");
-
-		        lista.add(trip1);
-		        lista.add(trip2);
-		        lista.add(trip3);
-		        lista.add(trip4);
-
-		    } catch (ParseException e) {
-		        System.out.println(e.getMessage());
-		    }
-
-		    return lista;
-		}
 		@Controller
 		public class HomeController {
 
 		    @GetMapping("/tabla")
 		    public String mostrarTabla(Model model) {
-		        List<Trip> lista = getTrips();
+		        List<Trip> lista = tripServices.buscarTodo();
 		        model.addAttribute("trips", lista);
 		        return "tabla";
 		    }
 		}
+		
+	
 	
 }
